7834311fff9c87ac976c811f0fd95fae1042439b
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-02-07 14:39:35 +0700

    Update Version 2.0.0
    - Can be set to work or not
    - added sound when hitting or archery hits the enemy

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <div align="center">
-<h1>KillDeathSound | v1.0.0<h1>
+<h1>KillDeathSound | v2.0.0<h1>
 </div>
 <p align="center">
 <a href="https://poggit.pmmp.io/p/KillDeathSound"><img src="https://poggit.pmmp.io/shield.state/KillDeathSound"></a>
@@ -12,25 +12,42 @@
 
 <br>
 
-## ☝ Features
+## Features
 - When the player is killed and dies, the sound will work
 
 <br>
 
-## ✍ Config
+## Config
 ```yaml
 ---
 # config.yml
-# You can see the list of sound effects at this link: https://www.digminecraft.com/lists/sound_list_pe.php
+#    
+#    ██╗░░██╗██╗██╗░░░░░██╗░░░░░██████╗░███████╗░█████╗░████████╗██╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+#    ██║░██╔╝██║██║░░░░░██║░░░░░██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██║░░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+#    █████═╝░██║██║░░░░░██║░░░░░██║░░██║█████╗░░███████║░░░██║░░░███████║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+#    ██╔═██╗░██║██║░░░░░██║░░░░░██║░░██║██╔══╝░░██╔══██║░░░██║░░░██╔══██║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+#    ██║░╚██╗██║███████╗███████╗██████╔╝███████╗██║░░██║░░░██║░░░██║░░██║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+#    ╚═╝░░╚═╝╚═╝╚══════╝╚══════╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+#
+# You can see the list of sound effects at this link: "https://www.digminecraft.com/lists/sound_list_pe.php"
+
 # Player Death
 death:
+  addsound: true
   sound: "random.explode" # Sound name
   volume: 1 # Volume sounds
   pitch: 1 # Pitch sounds
 
 # Player Kills
 kill:
+  addsound: true
   sound: "random.levelup" # Sound name
+  volume: 1 # Volume sounds
+  pitch: 1 # Pitch sounds
+# Player Hit & Archery
+hit:
+  addsound: true
+  sound: "random.orb" # Sound name
   volume: 1 # Volume sounds
   pitch: 1 # Pitch sounds
 ...
@@ -38,7 +55,13 @@ kill:
 
 <br>
 
-## ↓ Install
+## New
+- Can be set to work or not
+- added sound when hitting or archery hits the enemy
+
+<br>
+
+## Install
 - Step 1: Click the "Direct Download" button to download the plugin
 - Step 2: move the file "KillDeathSound.phar" into the file "plugins"
 - Step 3: Restart server for plugins to work
--- src/NoobMCBG/KillDeathSound/KillDeathSound.php
+++ src/NoobMCBG/KillDeathSound/KillDeathSound.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace NoobMCBG\KillDeathSound;
 
-use pocketmine\Server;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
@@ -13,37 +12,85 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 
 class KillDeathSound extends PluginBase implements Listener {
+    
+    /** @var KillDeathSound */
+	public static $instance;
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
         
 	public function onEnable() : void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
 		$this->checkUpdate();
+		self::$instance = $this;
 	}
 	
+	/** 
+    * @param bool $isRetry = false
+	*/
 	public function checkUpdate(bool $isRetry = false): void {
             $this->getServer()->getAsyncPool()->submitTask(new CheckUpdateTask($this->getDescription()->getName(), $this->getDescription()->getVersion()));
         }
-
-	public function onDeath(PlayerDeathEvent $ev){
-		$player = $ev->getPlayer();
+    
+    /**
+    * @param PlayerDeathEvent $event
+    */
+	public function onDeath(PlayerDeathEvent $event){
+		$player = $event->getPlayer();
 		if($player instanceof Player){
-			$soundName = $this->getConfig()->getAll()["death"]["sound"];
-			$volume = $this->getConfig()->getAll()["death"]["volume"];
-			$pitch = $this->getConfig()->getAll()["death"]["pitch"];
-			$this->PlaySound($player, $soundName, $volume, $pitch);
+			if($this->getConfig()->getAll()["death"]["addsound"] == true){
+			    $soundName = $this->getConfig()->getAll()["death"]["sound"];
+			    $volume = $this->getConfig()->getAll()["death"]["volume"];
+			    $pitch = $this->getConfig()->getAll()["death"]["pitch"];
+			    $this->PlaySound($player, $soundName, $volume, $pitch);
+			}
 		}
 		$cause = $player->getLastDamageCause();
 		if($cause instanceof EntityDamageByEntityEvent){
 			$damager = $cause->getDamager();
 			if($damager instanceof Player){
-				$soundName = $this->getConfig()->getAll()["kill"]["sound"];
+				if($this->getConfig()->getAll()["kill"]["addsound"] == true){
+				    $soundName = $this->getConfig()->getAll()["kill"]["sound"];
 			        $volume = $this->getConfig()->getAll()["kill"]["volume"];
 			        $pitch = $this->getConfig()->getAll()["kill"]["pitch"];
-				$this->PlaySound($player, $soundName, $volume, $pitch);
+				    $this->PlaySound($player, $soundName, $volume, $pitch);
+				}
 			}
 		}
 	}
-
+    
+    /**
+    * @param EntityDamageByEntityEvent $event
+    */
+	public function onHit(EntityDamageByEntityEvent $event){
+		$attacker = $event->getDamager();
+		$entity = $event->getEntity();
+		if($attacker instanceof Player){
+			if($this->getConfig()->getAll()["hit"]["addsound"] == true){
+		        $soundName = $this->getConfig()->getAll()["hit"]["sound"];
+			    $volume = $this->getConfig()->getAll()["hit"]["volume"];
+			    $pitch = $this->getConfig()->getAll()["hit"]["pitch"];
+                $this->PlaySound($attacker, $soundName, $volume, $pitch);
+            }
+		}
+		if($entity instanceof Player){
+			if($this->getConfig()->getAll()["hit"]["addsound"] == true){
+			    $soundName = $this->getConfig()->getAll()["hit"]["sound"];
+			    $volume = $this->getConfig()->getAll()["hit"]["volume"];
+			    $pitch = $this->getConfig()->getAll()["hit"]["pitch"];
+                $this->PlaySound($entity, $soundName, $volume, $pitch);
+            }
+		}
+	}
+    
+    /**
+    * @param Player $player
+    * @param string $soundName
+    * @param float $volume = 0
+    * @param float $pitch = 0
+    */
 	public function PlaySound(Player $player, string $soundName, float $volume = 0, float $pitch = 0) : void {
 		$packet = new PlaySoundPacket();
 		$packet->soundName = $soundName;
